e2b8664ce4568214bf30cd1143fffd3d1c2982fa
SalmaElhila <elhilasalma@gmail.com>
2023-07-25 16:47:15 -0700

    Betty main is fixed

--- Shell_main.c
+++ Shell_main.c
@@ -2,12 +2,16 @@
 #include <signal.h>
 
 /**
- * _main - Entry to launch program
+ * main - Entry point
+ *
+ * Description: 'Launch the shell'
  * @argc: argument counter
  * @argv: arguments
  * @env: environment var
+ *
  * Return: 1.
  */
+
 int main(int argc, char **argv, char **env)
 {
 	char *line;
